fe7e13e5806a2b3d09aeda8e5dc97a0506ec73b3
fahd shaykh <fahdshaykh@outlook.com>
2024-07-11 11:48:27 +0500

    admin panel embeded

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,8 +24,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => 'admin', //fake()->name()
+            'email' => 'admin@admin.com', //fake()->unique()->safeEmail()
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
---
+++ resources/views/dashboard/layouts/admin.blade.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- index.html  21 Nov 2019 03:44:50 GMT -->
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Movers and Packaers</title>
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="{{asset('admin/assets/css/app.min.css')}}">
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{asset('admin/assets/bundles/datatables/datatables.min.css')}}">
+  <link rel="stylesheet" href="{{asset('admin/assets/bundles/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="{{asset('admin/assets/bundles/bootstrap-daterangepicker/daterangepicker.css')}}">
+  <!-- Custom style CSS -->
+  <link rel="stylesheet" href="{{asset('admin/assets/css/components.css')}}">
+  <link rel="stylesheet" href="{{asset('admin/assets/css/style.css')}}">
+  <link rel="stylesheet" href="{{asset('admin/assets/css/custom.css')}}">
+  <link rel='shortcut icon' type='image/x-icon' href="{{asset('admin/assets/img/favicon.ico')}}" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+
+<body>
+  <div class="loader"></div>
+  <div id="app">
+    <div class="main-wrapper main-wrapper-1">
+      <div class="navbar-bg"></div>
+      <nav class="navbar navbar-expand-lg main-navbar sticky">
+        <div class="form-inline mr-auto">
+          <ul class="navbar-nav mr-3">
+            <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg
+									collapse-btn"> <i data-feather="align-justify"></i></a></li>
+            <li><a href="#" class="nav-link nav-link-lg fullscreen-btn">
+                <i data-feather="maximize"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <ul class="navbar-nav navbar-right">
+        <li class="dropdown"><a href="#" data-toggle="dropdown"
+              class="nav-link dropdown-toggle nav-link-lg nav-link-user"> <img alt="image" src="{{asset('admin/assets/img/user.png')}}"
+                class="user-img-radious-style"> <span class="d-sm-none d-lg-inline-block"></span></a>
+            <div class="dropdown-menu dropdown-menu-right pullDown">
+              <div class="dropdown-title">Welcome Admin</div>
+              <div class="dropdown-divider"></div>
+              <a href="{{ route('logout') }}" class="dropdown-item has-icon text-danger"
+                onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();"> <i class="fas fa-sign-out-alt"></i>
+                {{ __('Logout') }}
+              </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </div>
+          </li>
+        </ul>
+      </nav>
+      <div class="main-sidebar sidebar-style-2">
+        <aside id="sidebar-wrapper">
+          <div class="sidebar-brand">
+            <a href="/home"> <img alt="image" src="{{asset('admin/assets/img/logo.png')}}" class="header-logo" /> <span
+                class="logo-name">Wishing</span>
+            </a>
+          </div>
+          <ul class="sidebar-menu">
+            <li class="menu-header">Main</li>
+            <li class="active">
+              <a href="/home" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
+            </li>
+            <li class="">
+              <a class="nav-link" href="/contacts"><i data-feather="users"></i><span>Contacts</span></a>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="menu-toggle nav-link has-dropdown"><i
+                  data-feather="briefcase"></i><span>Categories</span></a>
+              {{-- <ul class="dropdown-menu">
+                <li><a class="nav-link" href="/bookings">Bookings</a></li>
+                <li><a class="nav-link" href="/category-create">Create category</a></li>
+                <li><a class="nav-link" href="/categories">Category list</a></li>
+              </ul> --}}
+            </li>
+          </ul>
+        </aside>
+      </div>
+      <!-- Main Content -->
+      <div class="main-content">
+        <section class="section">
+            <div class="section-body">
+                @yield('content')
+            </div>
+        </section>
+      </div>
+      <footer class="main-footer">
+        <div class="footer-left">
+          <a href="www.loopersolutions.com">LooperSolutions</a></a>
+        </div>
+        <div class="footer-right">
+        </div>
+      </footer>
+    </div>
+  </div>
+  <!-- General JS Scripts -->
+  <script src="{{asset('admin/assets/js/app.min.js')}}"></script>
+  <!-- JS Libraies -->
+  <script src="{{asset('admin/assets/bundles/jquery-ui/jquery-ui.min.js')}}"></script>
+  <!-- Template JS File -->
+  <script src="{{asset('admin/assets/js/scripts.js')}}"></script>
+  <!-- Custom JS File -->
+  <script src="{{asset('admin/assets/js/custom.js')}}"></script>
+  @yield('script')
+</body>
+
+</html>
